ca33dc6e52d0c6839a1a499016efee3d57d67525
Ronny <ronnygx@gmail.com>
2019-03-05 20:58:47 +0000

    Refactor: return empty couple if there arent 2 people

--- IncomprehensibleFinderKata/Finder.cs
+++ IncomprehensibleFinderKata/Finder.cs
@@ -9,28 +9,8 @@ namespace IncomprehensibleFinderKata {
         }
 
         public Couple Find(Filter filter) {
-            var couples = new List<Couple>();
-
-            for (var i = 0; i < persons.Count - 1; i++) {
-                for (var j = i + 1; j < persons.Count; j++) {
-                    var couple = new Couple();
-                    if (persons[i].BirthDate < persons[j].BirthDate) {
-                        couple.P1 = persons[i];
-                        couple.P2 = persons[j];
-                    }
-                    else {
-                        couple.P1 = persons[j];
-                        couple.P2 = persons[i];
-                    }
-
-                    couple.D = couple.P2.BirthDate - couple.P1.BirthDate;
-                    couples.Add(couple);
-                }
-            }
-
-            if (couples.Count < 1) {
-                return new Couple();
-            }
+            if (persons.Count < 2) return new Couple();
+            var couples = GetPossibleCouples();
 
             var answer = couples[0];
             foreach (var couple in couples) {
@@ -52,6 +32,29 @@ namespace IncomprehensibleFinderKata {
             }
 
             return answer;
+        }
+
+        private List<Couple> GetPossibleCouples() {
+            var couples = new List<Couple>();
+
+            for (var i = 0; i < persons.Count - 1; i++) {
+                for (var j = i + 1; j < persons.Count; j++) {
+                    var couple = new Couple();
+                    if (persons[i].BirthDate < persons[j].BirthDate) {
+                        couple.P1 = persons[i];
+                        couple.P2 = persons[j];
+                    }
+                    else {
+                        couple.P1 = persons[j];
+                        couple.P2 = persons[i];
+                    }
+
+                    couple.D = couple.P2.BirthDate - couple.P1.BirthDate;
+                    couples.Add(couple);
+                }
+            }
+
+            return couples;
         }
     }
 }
