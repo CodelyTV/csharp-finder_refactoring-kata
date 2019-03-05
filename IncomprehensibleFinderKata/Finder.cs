482e5dfd36cf2f91d7e05c920df1f4dbf288bfd2
Ronny <ronnygx@gmail.com>
2019-03-05 20:55:18 +0000

    Refactor: rename

--- IncomprehensibleFinderKata/Finder.cs
+++ IncomprehensibleFinderKata/Finder.cs
@@ -1,62 +1,52 @@
 ﻿using System.Collections.Generic;
 
-namespace IncomprehensibleFinderKata
-{
-    public class Finder
-    {
+namespace IncomprehensibleFinderKata {
+    public class Finder {
         private readonly List<Person> persons;
 
-        public Finder(List<Person> persons)
-        {
+        public Finder(List<Person> persons) {
             this.persons = persons;
         }
 
-        public Couple Find(Filter filter)
-        {
+        public Couple Find(Filter filter) {
             var couples = new List<Couple>();
 
-            for(var i = 0; i < persons.Count - 1; i++)
-            {
-                for(var j = i + 1; j < persons.Count; j++)
-                {
+            for (var i = 0; i < persons.Count - 1; i++) {
+                for (var j = i + 1; j < persons.Count; j++) {
                     var couple = new Couple();
-                    if(persons[i].BirthDate < persons[j].BirthDate)
-                    {
+                    if (persons[i].BirthDate < persons[j].BirthDate) {
                         couple.P1 = persons[i];
                         couple.P2 = persons[j];
                     }
-                    else
-                    {
+                    else {
                         couple.P1 = persons[j];
                         couple.P2 = persons[i];
                     }
+
                     couple.D = couple.P2.BirthDate - couple.P1.BirthDate;
                     couples.Add(couple);
                 }
             }
 
-            if(couples.Count < 1)
-            {
+            if (couples.Count < 1) {
                 return new Couple();
             }
 
-            Couple answer = couples[0];
-            foreach(var result in couples)
-            {
-                switch(filter)
-                {
+            var answer = couples[0];
+            foreach (var couple in couples) {
+                switch (filter) {
                     case Filter.Closest:
-                        if(result.D < answer.D)
-                        {
-                            answer = result;
+                        if (couple.D < answer.D) {
+                            answer = couple;
                         }
+
                         break;
 
                     case Filter.Furthest:
-                        if(result.D > answer.D)
-                        {
-                            answer = result;
+                        if (couple.D > answer.D) {
+                            answer = couple;
                         }
+
                         break;
                 }
             }
