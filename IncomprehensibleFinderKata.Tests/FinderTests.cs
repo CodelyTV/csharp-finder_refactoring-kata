0ac07a03e224ad10cb788901052da5ca59388afe
Ronny <ronnygx@gmail.com>
2019-03-05 20:38:47 +0000

    Refactor: rename class to "Person"

--- IncomprehensibleFinderKata.Tests/FinderTests.cs
+++ IncomprehensibleFinderKata.Tests/FinderTests.cs
@@ -4,14 +4,14 @@ using Xunit;
 
 namespace IncomprehensibleFinderKata.Tests {
     public class FinderTests {
-        private readonly Thing sue = new Thing() {Name = "Sue", BirthDate = new DateTime(1950, 1, 1)};
-        public readonly Thing greg = new Thing() {Name = "Greg", BirthDate = new DateTime(1952, 6, 1)};
-        private readonly Thing sarah = new Thing() {Name = "Sarah", BirthDate = new DateTime(1982, 1, 1)};
-        private readonly Thing mike = new Thing() {Name = "Mike", BirthDate = new DateTime(1979, 1, 1)};
+        private readonly Person sue = new Person() {Name = "Sue", BirthDate = new DateTime(1950, 1, 1)};
+        public readonly Person greg = new Person() {Name = "Greg", BirthDate = new DateTime(1952, 6, 1)};
+        private readonly Person sarah = new Person() {Name = "Sarah", BirthDate = new DateTime(1982, 1, 1)};
+        private readonly Person mike = new Person() {Name = "Mike", BirthDate = new DateTime(1979, 1, 1)};
 
         [Fact]
         public void Returns_Empty_Results_When_Given_Empty_List() {
-            var list = new List<Thing>();
+            var list = new List<Person>();
             var finder = new Finder(list);
 
             var result = finder.Find(FT.One);
@@ -22,7 +22,7 @@ namespace IncomprehensibleFinderKata.Tests {
 
         [Fact]
         public void Returns_Empty_Results_When_Given_One_Person() {
-            var list = new List<Thing>() {sue};
+            var list = new List<Person>() {sue};
             var finder = new Finder(list);
 
             var result = finder.Find(FT.One);
@@ -33,7 +33,7 @@ namespace IncomprehensibleFinderKata.Tests {
 
         [Fact]
         public void Returns_Closest_Two_For_Two_People() {
-            var list = new List<Thing>() {sue, greg};
+            var list = new List<Person>() {sue, greg};
             var finder = new Finder(list);
 
             var result = finder.Find(FT.One);
@@ -44,7 +44,7 @@ namespace IncomprehensibleFinderKata.Tests {
 
         [Fact]
         public void Returns_Furthest_Two_For_Two_People() {
-            var list = new List<Thing>() {greg, mike};
+            var list = new List<Person>() {greg, mike};
             var finder = new Finder(list);
 
             var result = finder.Find(FT.Two);
@@ -55,7 +55,7 @@ namespace IncomprehensibleFinderKata.Tests {
 
         [Fact]
         public void Returns_Furthest_Two_For_Four_People() {
-            var list = new List<Thing>() {greg, mike, sarah, sue};
+            var list = new List<Person>() {greg, mike, sarah, sue};
             var finder = new Finder(list);
 
             var result = finder.Find(FT.Two);
@@ -66,7 +66,7 @@ namespace IncomprehensibleFinderKata.Tests {
 
         [Fact]
         public void Returns_Closest_Two_For_Four_People() {
-            var list = new List<Thing>() {greg, mike, sarah, sue};
+            var list = new List<Person>() {greg, mike, sarah, sue};
             var finder = new Finder(list);
 
             var result = finder.Find(FT.One);
--- IncomprehensibleFinderKata/F.cs
+++ IncomprehensibleFinderKata/F.cs
@@ -4,8 +4,8 @@ namespace IncomprehensibleFinderKata
 {
     public class F
     {
-        public Thing P1 { get; set; }
-        public Thing P2 { get; set; }
+        public Person P1 { get; set; }
+        public Person P2 { get; set; }
         public TimeSpan D { get; set; }
     }
 }
