3a267f5e2104f717e1fdce4167a442cab443a658
Jose Antonio Arias Rodriguez <josearias210@gmail.com>
2020-01-28 02:10:54 -0400

    Evitar fecha de macimiento mayoresa la fecha actual

--- IncomprehensibleFinderKata/Person.cs
+++ IncomprehensibleFinderKata/Person.cs
@@ -15,12 +15,26 @@ namespace IncomprehensibleFinderKata
         public Person(string name, DateTime birthDate)
         {
             this.name = name;
-            this.BirthDate = birthDate;
+            this.BirthDate = birthDate.Date;
+            this.ValidateBirthDate();
         }
 
         public bool IsYoungerThan(Person person)
         {
             return this.BirthDate < person.BirthDate;
+        }
+
+        private void ValidateBirthDate()
+        {
+            if (this.IsBirthDateLessThanToday(this.BirthDate))
+            {
+                throw new ArgumentNullException("The birthDate cannot be greater than today's date");
+            }
+        }
+
+        private bool IsBirthDateLessThanToday(DateTime birthDate)
+        {
+            return DateTime.Now < birthDate;
         }
     }
 }
